5c90e1456d6a3261bb7271e9e1a54b25a95505e8
Patrick Sinco <psinco@gmail.com>
2014-09-19 07:19:01 -0500

    gets rects to appear in demo-bar-chart

--- js/demo-bar-chart.js
+++ js/demo-bar-chart.js
@@ -17,11 +17,11 @@ var svg = d3.select('.vis')
   .attr('transform', 'translate(' + margin.left + ',' +
     margin.top + ')');
 
-var xScale = d3.scale.ordinal()
-  .rangeBands([0, width]);
+var xScale = d3.scale.linear()
+  .range([0, width]);
 
-var yScale = d3.scale.linear()
-  .range([height, 0]);
+var yScale = d3.scale.ordinal()
+  .rangeBands([height, 0]);
 
 var xAxis = d3.svg.axis()
   .scale(xAxis)
@@ -51,20 +51,54 @@ d3.csv('data/tourn-1978/tourn-1978-32.csv', function(error, data) {
     return key != 'name' && key != 'id';
   });
 
-  xScale.domain(keys);
-
   // grab all the scores so we can determine min, max
   var scores = [];
   dataset.forEach(function(d) {
     for (var i = 0; i < keys.length; i++) {
-      scores.push(d[keys[i]]);
+      scores.push(d[keys[i]] - par);
     }
   });
 
+  // we could use d3.exent instead of min, max
   var minScore = d3.min(scores);
   var maxScore = d3.max(scores);
 
-  yScale.domain([minScore, maxScore]);
+  xScale.domain([minScore, 0, maxScore]);
+  yScale.domain(d3.range(0, data.length));
   
+  svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+      .attr('x', function(d) {
+        return xScale(d.r1 - par);
+      })
+      .attr('y', function(d, i) {
+        return yScale(i);
+      })
+      .attr('width', function(d) {
+        return xScale(d.r1 - par);
+      })
+      .attr('height', function(d) {
+        return yScale.rangeBand();
+      })
+      .style('fill', 'cadetblue')
+      
+  svg.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+      .attr('x', function(d) {
+        return width;
+      })
+      .attr('y', function(d, i) {
+        return yScale(i);
+      })
+      .attr('text-anchor', 'end')
+      .text(function(d) {
+        return d.name + ', ' + (d.r1 - par);
+      })
+      .style('fill', '#666')
+
 
 }); // end d3.csv
